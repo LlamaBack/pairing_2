71b16267807dbfcd11eadad781d7a2f2fc37d460
Jake Kim <imjakekim@gmail.com>
2022-06-08 12:58:59 -0700

    Add new condintional statement for other friends

--- friends.rb
+++ friends.rb
@@ -1,12 +1,14 @@
 best_friend = "caleb"
+very_good_friend = "michael"
+
 
 if best_friend == "caleb"
-  puts "whats up"
+  puts "hi caleb"
+  else if very_good_friend == "michael"
+    puts "hello michael"
+  end
 end
 
 if "michael" == "michael"
   puts "sup"
 end
-
-
-very_good_friend = "michael"
